cededac58c7a6a641c1bb201b3031818edbb7d9d
daltonmoore <daltonraymoore@gmail.com>
2021-01-05 15:38:01 -0500

    re-add project

---
+++ Into the Shadows/Assets/Scripts/ligthheal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ligthheal : MonoBehaviour
+{
+    player player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<player>(); ;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player.inHealingLight = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player.inHealingLight = false;
+        }
+    }
+}
