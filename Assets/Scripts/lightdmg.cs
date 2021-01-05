ddadb0443f04506a0b38228dd4305bab3fd3fc39
daltonmoore <daltonraymoore@gmail.com>
2021-01-05 15:46:10 -0500

    re-add again

---
+++ Assets/Scripts/lightdmg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class lightdmg : MonoBehaviour
+{
+    player player;
+    Light2D light2D;
+    bool lightOn;
+    Coroutine lightTimerCoroutine;
+    float lightTimer = 2f;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<player>();
+        light2D = GetComponent<Light2D>();
+    }
+
+    private void Update()
+    {
+        if (lightTimerCoroutine == null)
+        {
+            lightTimerCoroutine = StartCoroutine(LightTimer());
+        }
+    }
+
+    IEnumerator LightTimer()
+    {
+        yield return new WaitForSeconds(lightTimer);
+        lightOn = !lightOn;
+        light2D.
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player.inLight = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player.inLight = false;
+        }
+    }
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour
+{
+    public bool inLight, inHealingLight;
+    [SerializeField]
+    int totalHealth;
+    int health;
+    float lightDmgCoolDown = .01f;
+    float lightHealCoolDown = .02f;
+    Coroutine lightDmgCoolDownCoroutine, lightHealCoolDownCoroutine;
+    Canvas canvas;
+    Slider healthBar;
+
+    void Start()
+    {
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        healthBar = canvas.transform.Find("Healthbar").GetComponent<Slider>();
+        health = totalHealth;
+        healthBar.maxValue = totalHealth;
+        healthBar.value = healthBar.maxValue;
+    }
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(x * .01f, y * .01f);
+
+        if (inLight && lightDmgCoolDownCoroutine == null)
+        {
+            HitByDmgLight();
+            lightDmgCoolDownCoroutine = StartCoroutine(LightDmgCoolDown());
+        }
+
+        if (inHealingLight && lightHealCoolDownCoroutine == null)
+        {
+            HitByHealLight();
+            lightHealCoolDownCoroutine = StartCoroutine(LightHealCoolDown());
+        }
+    }
+
+    IEnumerator LightDmgCoolDown()
+    {
+        yield return new WaitForSeconds(lightDmgCoolDown);
+        lightDmgCoolDownCoroutine = null;
+    }
+
+    IEnumerator LightHealCoolDown ()
+    {
+        yield return new WaitForSeconds(lightHealCoolDown);
+        lightHealCoolDownCoroutine = null;
+    }
+
+    public void HitByDmgLight()
+    {
+        if (health > 0)
+        {
+            health--;
+            healthBar.value = (float)health / totalHealth * totalHealth;
+        }
+    }
+
+    public void HitByHealLight()
+    {
+        if (health < totalHealth)
+        {
+            health++;
+            healthBar.value = (float)health / totalHealth * totalHealth;
+        }
+    }
+}
