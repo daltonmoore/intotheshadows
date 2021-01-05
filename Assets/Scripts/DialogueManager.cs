8babe7a56fcf2b80c1bb0c9b11686f7aae7fac34
Gaige <57609988+GManMT540@users.noreply.github.com>
2021-01-05 18:58:30 -0500

    Dialogue

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public Text nameText;
+    public Text dialogueText;
+    public Dialogue dialogue;
+    
+    public AudioSource audioSource;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp("e"))
+        {
+            DisplayNextSentence();
+        }
+    }
+
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation with " + dialogue.name);
+
+        this.dialogue = dialogue;
+
+        animator.SetBool("IsOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopCoroutine(TypeSentence(sentence));
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            audioSource.Play();
+            dialogueText.text += letter;
+            yield return null;
+            audioSource.Stop();
+        }
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of Conversation");
+
+        dialogue.interactions++;
+
+        animator.SetBool("IsOpen", false);
+    }
+
+}
