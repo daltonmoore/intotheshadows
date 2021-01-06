62ca546344d04206659616e5b0be0d61e973dca4
daltonmoore <daltonraymoore@gmail.com>
2021-01-05 19:53:33 -0500

    change dialogue to be hard coded in code

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -13,4 +13,5 @@ public class Dialogue
     [TextArea(3, 10)]
     public string[] sentences;
 
+    public bool interactable = true;
 }
--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -45,6 +45,93 @@ public class DialogueManager : MonoBehaviour
 
         sentences.Clear();
 
+        #region Get Sentences
+        if (dialogue.name == "Shadow Child")
+        {
+            switch (dialogue.interactions)
+            {
+                case 0:
+                    dialogue.sentences = new[]
+                    {
+                        "Hello…",
+                        "I can’t move, the light will find me…",
+                        "I can’t move, the light will find me…"
+                    };
+                    break;
+                case 1:
+                    dialogue.sentences = new[]
+                    {
+                        "I can’t move, the light will find me…"
+                    };
+                    break;
+            }
+        }
+        else if (dialogue.name == "Curvy Shadow")
+        {
+            switch (dialogue.interactions)
+            {
+                case 0:
+                    dialogue.sentences = new[]
+                    {
+                        "Hello, Darling.",
+                        "Need anything? How about you just stay right here and I’ll help you relax…"
+                    };
+                    break;
+                case 1:
+                    dialogue.sentences = new[]
+                    {
+                        "Sigh… everybody always leaves and gets caught by the light…"
+                    };
+                    break;
+            }
+        }
+        else if (dialogue.name == "Shadow Parent")
+        {
+            switch (dialogue.interactions)
+            {
+                case 0:
+                    dialogue.sentences = new[]
+                    {
+                        "If you see my two children could you bring them back home?",
+                        "They wandered off and I’m afraid the light has found them…"
+                    };
+                    break;
+                case 1:
+                    dialogue.sentences = new[]
+                    {
+                        "Did you find my children?"
+                    };
+                    break;
+            }
+        }
+        else if (dialogue.name == "Shadow Villager 1")
+        {
+            switch (dialogue.interactions)
+            {
+                case 0:
+                    dialogue.sentences = new[]
+                    {
+                        "I try not to leave the village. The light never comes here.",
+                        "You’re a brave shadow if you leave this place."
+                    };
+                    break;
+            }
+        }
+        else if (dialogue.name == "Shadow Villager 2")
+        {
+            switch (dialogue.interactions)
+            {
+                case 0:
+                    dialogue.sentences = new[]
+                    {
+                        "The Mother of Shadows told me she loves all of her little shadows.",
+                        "Do you think she means it?"
+                    };
+                    break;
+            }
+        }
+        #endregion
+
         foreach (string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
@@ -84,8 +171,25 @@ public class DialogueManager : MonoBehaviour
         Debug.Log("End of Conversation");
 
         dialogue.interactions++;
-
         animator.SetBool("IsOpen", false);
+
+        SetInteractable();
+    }
+
+    void SetInteractable()
+    {
+        if (dialogue.interactions > 3 && dialogue.name == "Light Worshiper")
+        {
+            dialogue.interactable = false;
+        }
+        else if (dialogue.interactions > 1 && dialogue.name == "Shadow Villager 1")
+        {
+            dialogue.interactable = false;
+        }
+        else if (dialogue.interactions > 1 && dialogue.name == "Shadow Villager 2")
+        {
+            dialogue.interactable = false;
+        }
     }
 
 }
