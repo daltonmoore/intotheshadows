191be723ed0ea1ba525c8340c3c14f95864c8a74
Gaige <57609988+GManMT540@users.noreply.github.com>
2021-01-22 10:51:49 -0500

    Audio & Options

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -2,13 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class Options : MonoBehaviour
 {
     public SceneClass sceneClass;
 
+    public AudioMixer audioMixer;
+
     public void BackBttn()
     {
         SceneManager.UnloadSceneAsync("Options");
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVol", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVol", volume);
+    }
+
+    public void SetSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("SoundVol", volume);
     }
 }
